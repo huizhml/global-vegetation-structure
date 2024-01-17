d26bf4e5befe434697c1804fd29b7e2f07c4d826
Hui Zhang <omegazhanghui@gmail.com>
2024-01-17 11:51:42 +0200

    smaller n_cores

--- download/s2_download_config_gen.py
+++ download/s2_download_config_gen.py
@@ -10,7 +10,7 @@ with open(data_folder / 'download_config.txt', 'a') as f:
         if n_partitions ==0:
             continue
         print(n_partitions)
-        n_cores = math.floor(math.log(n_partitions, 2)) 
+        n_cores = math.floor(math.log(n_partitions, 2)) / 2
         n_cores= min(2**n_cores, 128) # maximum #cores we can get
         n_parallel = n_cores + 20
         f.write(f'{zone} {n_partitions} {n_cores} {n_parallel} \n')
