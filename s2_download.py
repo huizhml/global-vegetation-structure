1134c2c7d9961b84b185b16d8a972c13e2237423
Hui Zhang <omegazhanghui@gmail.com>
2024-01-24 23:28:24 +0200

    drop loc with defective cover > 0.9

--- s2_download.py
+++ s2_download.py
@@ -379,13 +379,13 @@ class S2Downloader:
         # even patch is computed, patch.isin().sum() will still be lazy
         scl = patch.data
         defective_cover = np.any([(scl == k) for k in defective_SCL], 0).sum((-2,-1)) / np.prod(scl.shape[-2:])
+        if np.isnan(defective_cover).all() or defective_cover.min() > 0.9:
+            return
         patch_df = pd.DataFrame({
             'id': patch.id.values,
             'defective_cover': defective_cover.squeeze(),
             'delta_day': [np.abs(t - pd.Timestamp(date)).days for t in patch.time.values],
         })
-        if patch_df.empty or patch_df['defective_cover'].isna().all():
-            return None
         
         patch_df = patch_df.sort_values(['defective_cover', 'delta_day'])
         best = patch_df.iloc[0]
