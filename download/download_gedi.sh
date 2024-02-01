6ff055cac7cd9851c99edd2f063f1a28c5f41e36
Hui Zhang <omegazhanghui@gmail.com>
2024-02-01 16:54:55 +0200

    sample from server

--- download/download_gedi.sh
+++ download/download_gedi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=project_465000894
 #SBATCH --partition=small
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=160G
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32G
 #SBATCH --time=3-00:00:00
 #SBATCH --job-name=GEDI-download
 #SBATCH --output=./logs/%x-%j.out
@@ -10,4 +10,4 @@
 
 export KEY_FILE=$2
 singularity run -B /project/project_465000894,/scratch/project_465000894,/flash/project_465000894 ~/project/pytorch_latest.sif \
-            python -u -m download.gedi init.year=$1 init.n_parallel=16
+            python -u -m download.gedi init.year=$1
