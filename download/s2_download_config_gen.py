95bd31821d6f0f3ad63e6f53b7743103ac8922ff
Hui Zhang <omegazhanghui@gmail.com>
2024-02-11 04:38:13 +0200

    one parquet file per zone causes large mem consume

--- download/s2_download_config_gen.py
+++ download/s2_download_config_gen.py
@@ -15,17 +15,6 @@ def repartition(year_folder, zone, partition_size='128K'):
     ddf = ddf.repartition(partition_size=partition_size)
     ddf.to_parquet(year_folder / f'{zone}', name_function=lambda x: f'partition_{x}.parquet')
 
-@dask.delayed
-def merge_orbits(year_folder, zone):
-    files = list((year_folder / zone).glob('partition*.parquet'))
-    schema = pq.ParquetFile(files[0]).schema_arrow
-    with pq.ParquetWriter(year_folder / f"{zone}.parquet", schema=schema) as writer:
-        for file in files:
-            writer.write_table(pq.read_table(file, schema=schema))
-    for file in files:
-        os.remove(file)
-    return zone
-
 def aggregate_zones(group):
     if group.index[0] <= 16:
         zones = ','.join(group['zone'])
@@ -36,30 +25,17 @@ def aggregate_zones(group):
     else:
         return group
 
-def gen_s2_download_config(data_folder, save_dir, year):
-    s2_data_dir = Path.home() / save_dir
-    config = []
-    for file in data_folder.glob('*.parquet'):
-        zone = file.stem
-        # check if this zone has s2 data downloaded
-        if (s2_data_dir / f'{zone}_{year}_done').exists():
-            continue
-        try:
-            num_row_groups = pq.read_metadata(file).num_row_groups
-        except:
-            logger.info(file)
-        if num_row_groups >=2000:
-            n_cores = 64
-        else:
-            n_cores = math.floor(math.log(num_row_groups, 2)) // 2
-            n_cores = max(n_cores, 1)
-            n_cores= min(2**n_cores, 128) # maximum #cores we can get
-        n_parallel = n_cores * 2 + 2
-        config.append([n_cores, num_row_groups, n_parallel, zone])
-    df = pd.DataFrame(config, columns=['ncores', 'npartitions', 'nparallel', 'zone'])
-    df = df.set_index('ncores').groupby('ncores').apply(aggregate_zones)
-    df = df.reset_index().drop(columns='level_1')
-    df.to_csv(data_folder /'download_config.csv', index=False, sep=';')
+def gen_config_for_zone(zone_folder, year):
+    npartitions = len(list(zone_folder.glob('partition*.parquet')))
+    if npartitions >=500:
+        n_cores = 64
+    else:
+        n_cores = math.floor(math.log(npartitions, 2)) // 2
+        n_cores = max(n_cores, 1)
+        n_cores= min(2**n_cores, 128) # maximum #cores we can get
+    n_parallel = n_cores * 2
+
+    return n_cores, npartitions, n_parallel
 
 
 @hydra.main(config_path='../config', config_name='s2_download', version_base='1.2')
@@ -74,21 +50,24 @@ def main(cfg):
 
     for year in years:
         data_folder = Path.home() / cfg.data_dir/ str(year)
-        futures = []
+        config = []
         for zone in os.listdir(data_folder):
             if not os.path.isdir(data_folder / zone) or (data_folder/f'{zone}.parquet').exists():
                 continue
             logger.info(f'repartition {zone}...')
-            repartition(data_folder, zone)
-            logger.info(f'merge {zone}...')
-            future = client.compute(merge_orbits(data_folder, zone))
-            futures.append(future)
+            repartition(data_folder, zone, cfg.partition_size)
 
-        #res = client.gather(futures)
-        logger.info(f'generate download config for {year}...') 
-        gen_s2_download_config(data_folder, cfg.save_dir, year)
+            s2_data_dir = Path.home() / cfg.save_dir
+            if (s2_data_dir / f'{zone}_{year}_done').exists():
+                continue
+            logger.info(f'generate download config for {zone} {year}...')
+            line = gen_config_for_zone(data_folder/zone, year)
+            config.append(line+[zone])
+    df = pd.DataFrame(config, columns=['ncores', 'npartitions', 'nparallel', 'zone'])
+    df = df.set_index('ncores').groupby('ncores').apply(aggregate_zones)
+    df = df.reset_index().drop(columns='level_1')
+    df.to_csv(data_folder /'download_config.csv', index=False, sep=';')
     client.close()
-
 
 if __name__ == '__main__':
     main()
