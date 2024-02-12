28e5dbb13de40a0b5a1ba1c7d007295fb9535f0c
Hui Zhang <omegazhanghui@gmail.com>
2024-02-12 10:11:35 +0200

    merge h5 files with ExternalLink

--- download/merge_h5s.py
+++ download/merge_h5s.py
@@ -7,16 +7,10 @@ def merge_h5_files(file_list, output_file):
         for file in file_list:
             group_name = os.path.basename(file).split('.')[0]  # Create a group name based on file name
             h5_out[group_name] = h5py.ExternalLink(file, '/')
-            # with h5py.File(file, 'r') as h5_in:
-            #     group = h5_out.create_group(group_name)
-            #     # Copy all datasets from the current file to the group
-            #     for dset in h5_in:
-            #         for p in h5_in[dset]:
-            #             group[dset] = h5_in[dset][...]
 
-# file_list = (Path.home() / 'data/GEDI/01G').glob('partition_*.h5')
-file_list = ['32M', '32N', '33M', '33P']
+# file_list = (Path.home() / 'data/GEDI').glob('*.h5')
+file_list = ['32M', '32N', '33M', '33P', '32P', '33N']
 file_list = [Path.home() / 'data/GEDI' /f'{f}.h5' for f in file_list]
-output_file = Path.home() / 'data/GEDI/merged_file_test.h5'  # Name of the output merged file
+output_file = Path.home() / 'data/GEDI/merged_file.h5'  # Name of the output merged file
 
 merge_h5_files(file_list, output_file)
