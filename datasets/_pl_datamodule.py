fc97dab5f225a5e57cbcf1c0cfdb6f32396eaa61
Hui Zhang <omegazhanghui@gmail.com>
2024-04-30 16:24:33 +0300

    refactor dataset

--- datasets/_pl_datamodule.py
+++ datasets/_pl_datamodule.py
@@ -1,11 +1,10 @@
 import os
 import logging
 from pathlib import Path
-from typing import Callable, List, Union
+from typing import List, Union
 import pandas as pd
 import torch
-import lightning.pytorch as pl
-import tables
+import lightning as L
 import h5py
 from datatree.io import _iter_nc_groups
 from h5netcdf.legacyapi import Dataset as h5Dataset
@@ -14,16 +13,21 @@ from datasets.s2 import S2Dataset
 
 logger = logging.getLogger(__name__)
 
-class PLDataModel(pl.LightningDataModule):
-    TEST_ZONES = ['32M', '01G']
+class PLDataModel(L.LightningDataModule):
+    TEST_ZONES = ['23L', '33N', '33P']
     TROPIC_SIX = ['32M', '32N', '32P', '33M', '33N', '33P']
     
     def __init__(self, 
-        cache_dir: str,
-        data_dir: str,
-        split_files_dir: str,
-        h5_file: str,
+        cache_dir: str=None,
+        data_dir: str=None,
+        h5_dir: str=None,
+        split_files_dir: str=None,
+        index_table_file: str=None,
+        merged_h5_file: str=None,
+        test_data_name: str='cal',
         use_zones:Union[str, List[str]]='TEST_ZONES',
+        val_ratio: float=0.2,
+        random_state: int=42,
         batch_size: int=64,
         num_workers: int=8,
         pin_memory: bool=True,
@@ -32,111 +36,122 @@ class PLDataModel(pl.LightningDataModule):
         **kwargs
         ):
         super().__init__()
-        data_dir = Path.home() / data_dir
-        h5_file = data_dir / h5_file if isinstance(h5_file, str) else h5_file
-        if not h5_file.exists():
-            logger.info('Merging h5 files...')
-            self._merge_h5_files(use_zones, h5_file)
+        data_dir = Path(data_dir).expanduser()
+        index_table_file = Path(index_table_file).expanduser()
+        split_files_dir = Path(split_files_dir).expanduser()
+        h5_dir = data_dir / h5_dir
+        self.merged_h5_file = data_dir / merged_h5_file
+        self.cache_dir = Path(cache_dir)
+        self.cache_dir.mkdir(parents=True, exist_ok=True)
+        self.test_data_name = test_data_name
 
-        if cache_dir:
-            self.cache_dir = Path(cache_dir)
-            self.cache_dir.mkdir(parents=True, exist_ok=True)
-            if not (self.cache_dir / 'train_index_table.csv').exists():
-                logger.info('Generating index tables for train,val,cal and test dataset...')
-                self._gen_index_table(h5_file, data_dir / split_files_dir)   
-
-        self.train_dataset = S2Dataset(h5_file, index_table=self.cache_dir / 'train_index_table.csv')
-        self.val_dataset = S2Dataset(h5_file, index_table=self.cache_dir / 'val_index_table.csv')
-        self.test_dataset = S2Dataset(h5_file, index_table=self.cache_dir / 'cal_index_table.csv')
-
+        self._merge_h5_files(use_zones, h5_dir)
+        self._gen_index_table(index_table_file)
+        self.train_cal_test_split(index_table_file, split_files_dir)
+        
+        self.val_ratio = val_ratio
+        self.random_state = random_state
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.pin_memory = pin_memory
         self.shuffle = shuffle
         self.drop_last = drop_last
 
-    def _gen_index_table(self, h5_file:Path, split_files_dir, save=True):
+
+    def _gen_index_table(self, index_table_file, save=True):
         """
-        Generate index tables for train, validation, calibration, and test datasets.
+        Go through all groups in the HDF5 file and generate index table.
 
         Args:
-            split_files_dir (str): The directory containing the split files.
+            index_table_file (str): The overall index table for zones used.
             save (bool, optional): Whether to save the index tables as CSV files. Defaults to True.
 
         Returns:
             None
         """
-
-        val_tiles_df = pd.read_csv(split_files_dir / "val_tiles.txt", sep=" ", names=['s2_tile'])
-        cal_tiles_df = pd.read_csv(split_files_dir / "cal_tiles.txt", sep=" ", names=['s2_tile'])
-        test_tiles_df = pd.read_csv(split_files_dir / "test_tiles.txt", sep=" ", names=['s2_tile'])
-        train_index_table = []
-        val_index_table = []
-        cal_index_table = []
-        test_index_table = []
-
-        with h5Dataset(h5_file, mode='r') as ncds:
-            with h5py.File(h5_file) as data:
+        #TODO: use dask for larger data?
+        if index_table_file.exists():
+            logger.info('index table exists, skipping...')
+            return
+        logger.info('Generating index tables for all zones used...')
+        index_df = []
+        with h5Dataset(self.merged_h5_file, mode='r') as ncds:
+            index_df = []
+            with h5py.File(self.merged_h5_file) as data:
                 for group in _iter_nc_groups(ncds):
-                    if len(group.split('/')) == 4: #TODO: not eligant
-                        _, zone, year, partition = group.split('/')
-                        partition = int(partition)
-                        val_tiles = val_tiles_df[val_tiles_df['s2_tile'].str.contains(zone)].values
-                        cal_tiles = cal_tiles_df[cal_tiles_df['s2_tile'].str.contains(zone)].values
-                        test_tiles = test_tiles_df[test_tiles_df['s2_tile'].str.contains(zone)].values
-                        
-                        s2_ids = data[f'{zone}/{year}/{partition}/id'][:].astype('U')
-                        in_partition_idx_val = []
-                        in_partition_idx_cal = []
-                        in_partition_idx_test = []
-                        in_partition_idx_train = []
-                        for i, s2_id in enumerate(s2_ids):
-                            s2_id = s2_id[33:38]
-                            if s2_id in val_tiles:
-                                in_partition_idx_val.append(i)
-                            elif s2_id in cal_tiles:
-                                in_partition_idx_cal.append(i)
-                            elif s2_id in test_tiles:
-                                in_partition_idx_test.append(i)
-                            else:
-                                in_partition_idx_train.append(i)
-                        val_index_table.extend([(zone, year, partition, idx) for idx in in_partition_idx_val])
-                        cal_index_table.extend([(zone, year, partition, idx) for idx in in_partition_idx_cal])
-                        test_index_table.extend([(zone, year, partition, idx) for idx in in_partition_idx_test])
-                        train_index_table.extend([(zone, year, partition, idx) for idx in in_partition_idx_train])
+                    if len(group.split('/')) == 4:
+                        s2_ids = data[f'{group}/id'][:].astype('U')
+                        index_df.append([group, s2_ids])
+        index_df = pd.DataFrame(index_df, columns=['path','s2_tile'])
+        index_df = index_df.explode('s2_tile')
+        index_df['s2_tile'] = index_df['s2_tile'].str[33:38]
+        index_df['in_partition_idx'] = index_df.groupby('path').cumcount()
 
-        for index_table, name in zip([train_index_table, val_index_table, cal_index_table, test_index_table], ['train', 'val', 'cal', 'test']):
-            index_table = pd.DataFrame(index_table, columns=['zone', 'year', 'partition_idx', 'in_partition_idx'])
-            if save:
-                index_table.to_csv(self.cache_dir / f'{name}_index_table.csv')
-                print(f'{name} index table saved to: ', self.cache_dir / f'{name}_index_table.csv')
+        if save:
+            index_df.to_csv(index_table_file)
+            print(f'index table saved to: ', index_table_file)
 
-    def _merge_h5_files(self, use_zones, output_file):
-            """
-            Merges the HDF5 files for the specified zones into a single HDF5 file.
+    def train_cal_test_split(self, index_table_file, split_files_dir):
+        """
+        Split the index table into train, cal, and test datasets.
 
-            Parameters:
-            use_zones (str or list): The zones to merge. If 'all', merges all zones in the directory.
-                                    If a list, merges the specified zones.
-            output_file (str): The path to the output HDF5 file.
+        Args:
+            index_table_file (str): The path to the index table.
+            split_files_dir (str): The path to the file containing the test tiles.
 
-            Returns:
-            tables.File: The merged HDF5 file opened in read mode.
-            """
-            
-            if isinstance(use_zones, list):
-                zones = use_zones
-            elif use_zones == 'all':
-                zones = [f for f in os.listdir(output_file.parent) if f.is_dir()]
-            else:
-                zones = self.__getattribute__(use_zones.upper())
-            
-            with h5py.File(output_file, 'w') as h5_out:
-                for zone in zones:
-                    h5_out[zone] = h5py.ExternalLink(output_file.parent/f'{zone}.h5', '/')
+        Returns:
+            None
+        """
+        # Make sure the train dataset doesn't include any images from the cal and test splits
+        if  (self.cache_dir / 'cal_index_table.csv').exists() and (self.cache_dir / 'test_index_table.csv').exists() and (self.cache_dir / 'train_index_table.csv').exists():
+            logger.info('Splitting index tables for train, cal, test datasets...')
+            cal_tiles_df = pd.read_csv(split_files_dir / "cal_tiles.txt", sep=" ", names=['s2_tile'])
+            test_tiles_df = pd.read_csv(split_files_dir / "test_tiles.txt", sep=" ", names=['s2_tile'])
+            index_df = pd.read_csv(index_table_file)
+
+            cal_index_df = index_df[index_df['s2_tile'].isin(cal_tiles_df['s2_tile'])]
+            test_index_df = index_df[index_df['s2_tile'].isin(test_tiles_df['s2_tile'])]
+            train_index_df = index_df[~index_df['s2_tile'].isin(cal_tiles_df['s2_tile']) & ~index_df['s2_tile'].isin(test_tiles_df['s2_tile'])]
+            val_index_df = train_index_df.sample(frac=self.val_ratio, random_state=self.random_state)
+            train_index_df = train_index_df.drop(val_index_df.index)
+            train_index_df.to_csv(self.cache_dir / 'train_index_table.csv')
+            val_index_df.to_csv(self.cache_dir / 'val_index_table.csv')
+            cal_index_df.to_csv(self.cache_dir / 'cal_index_table.csv')
+            test_index_df.to_csv(self.cache_dir / 'test_index_table.csv')
+    
+    def _merge_h5_files(self, use_zones, h5_dir):
+        """
+        Merges the HDF5 files for the specified zones into a single HDF5 file.
+
+        Parameters:
+        use_zones (str or list): The zones to merge. If 'all', merges all zones in the directory.
+                                If a list, merges the specified zones.
+        h5_dir (Path): The directory containing the HDF5 files.
+
+        """
+        
+        if self.merged_h5_file.exists():
+            # TODO: check if existing merged file contains exactly the same zones
+            logger.info('h5 files have been merged, skipping...')
+            return
+        
+        logger.info('Merging h5 files...')
+        if isinstance(use_zones, list):
+            zones = use_zones
+        elif use_zones == 'all':
+            zones = list(h5_dir.glob('*.h5'))
+            zones = [zone.stem for zone in zones]
+        else:
+            zones = self.__getattribute__(use_zones.upper())
+        
+        with h5py.File(self.merged_h5_file, 'w') as h5_out:
+            for zone in zones:
+                h5_out[zone] = h5py.ExternalLink(h5_dir/f'{zone}.h5', '/')
 
 
     def train_dataloader(self):
+        if not hasattr(self, 'train_dataset'):
+            self.train_dataset = S2Dataset(self.merged_h5_file, index_table=self.cache_dir / 'train_index_table.csv')
         return torch.utils.data.DataLoader(
             self.train_dataset,
             batch_size=self.batch_size,
@@ -147,6 +162,8 @@ class PLDataModel(pl.LightningDataModule):
         )
 
     def val_dataloader(self):
+        if not hasattr(self, 'val_dataset'):
+            self.val_dataset = S2Dataset(self.merged_h5_file, index_table=self.cache_dir / 'val_index_table.csv')
         return torch.utils.data.DataLoader(
             self.val_dataset,
             batch_size=self.batch_size,
@@ -157,6 +174,8 @@ class PLDataModel(pl.LightningDataModule):
         )
 
     def test_dataloader(self):
+        if not hasattr(self, 'test_dataset'):
+            self.test_dataset = S2Dataset(self.merged_h5_file, index_table=self.cache_dir / f'{self.test_data_name}_index_table.csv')
         return torch.utils.data.DataLoader(
             self.test_dataset,
             batch_size=self.batch_size,
@@ -171,7 +190,7 @@ if __name__ == '__main__':
     import hydra
     @hydra.main(config_name='train', config_path='../config', version_base='1.2')
     def main(cfg):
-        datamodel = PLDataModle(**cfg.data.init_args)
+        datamodel = PLDataModel(**cfg.data.init_args)
         for img, label in datamodel.train_dataloader():
             print(img.shape, label.shape)
 
