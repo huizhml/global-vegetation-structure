Use torchmetrics, correct metric averaging

models/_base_pl_model.py
@@ -21,6 +21,7 @@ class BaseModel(L.LightningModule):
             feed_slope: bool = False,
             zero_slope: bool = False,
             feed_latlon: bool = False,
+            predict_high_slope: bool = False,
             loss_fc: nn.Module = None, 
             transform: nn.Module = None, 
             yhat_transform: nn.Module = None, *args: Any, **
@@ -29,6 +30,7 @@ class BaseModel(L.LightningModule):
         self.feed_slope = feed_slope
         self.zero_slope = zero_slope
         self.feed_latlon = feed_latlon
+        self.predict_high_slope = predict_high_slope
         self.loss_fc = loss_fc
         self.transform = transform
         self.yhat_transform = yhat_transform
@@ -84,6 +86,14 @@ class BaseModel(L.LightningModule):
 
         return latitudes, longitudes
 
+    def on_train_epoch_start(self):
+        self.loss_fc.reset()
+        return super().on_train_epoch_start()
+    
+    def on_validation_epoch_start(self):
+        self.loss_fc.reset()
+        return super().on_validation_epoch_start()
+
     def training_step(self, sample, batch_idx):
         x = self.transform(sample[0])
         if self.feed_slope:
@@ -127,7 +137,7 @@ class BaseModel(L.LightningModule):
             x = torch.cat([x,lat.unsqueeze(1), sin_lon.unsqueeze(1), cos_lon.unsqueeze(1)], dim=1)
         y_hat = self.forward(x.float())
         
-        error_metrics, error_metrics_veg, output = self.loss_fc(y_hat, *sample[1:])
+        error_metrics, error_metrics_veg, output = self.loss_fc(y_hat, *sample[1:], predict_high_slope=self.predict_high_slope)
         for name, err in error_metrics.items():
             self.log(f'val_{name}', err, on_epoch=True, on_step=False, sync_dist=True)
         for name, err in error_metrics_veg.items():

models/losses/base.py
@@ -1,16 +1,59 @@
+from typing import List
 import torch
 import torch.nn as nn
 from torch import Tensor
+from torchmetrics import Metric
 from const import ESA_WC
+
+class RegressionMetrics(Metric):
+    def __init__(self, individual_rhs:List[int]=[98, 100], **kwargs):
+        super().__init__(**kwargs)
+        self.individual_rhs = individual_rhs
+        self.add_state("n", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("sse", default=torch.zeros(101), dist_reduce_fx="sum")
+        self.add_state("se", default=torch.zeros(101), dist_reduce_fx="sum")
+        self.add_state("sae", default=torch.zeros(101), dist_reduce_fx="sum")
+
+
+    def update(self, residuals: Tensor) -> None:
+        self.n += residuals.shape[0]
+        self.se += residuals.sum(dim=0) # ME
+        self.sae += residuals.abs().sum(dim=0) # MAE
+        self.sse += residuals.pow(2).sum(dim=0)  # MSE, RMSE
+
+    def compute(self) -> Tensor:
+        mse = self.sse.mean() / self.n
+        metrics = dict(
+            mse = mse,
+            rmse = torch.sqrt(mse),
+            me = self.se.mean() / self.n,
+            mae = self.sae.mean() / self.n
+        )
+        for idx in self.individual_rhs:
+            metrics.update({
+                f'rmse_rh{idx}': torch.sqrt(self.sse[idx] / self.n),
+                f'me_rh{idx}': self.se[idx] / self.n,
+                f'mae_rh{idx}': self.sae[idx] / self.n
+            })
+        return metrics
+
+
 
 class MaskedLoss(nn.BCELoss):
 
-    def __init__(self, name:str='mae', zero_out:bool=False, slope_th: float = 20) -> None:
+    def __init__(self, name:str='mae', zero_out:bool=False, slope_th: float = 20, individual_rhs:List[int]=[98, 100]) -> None:
         super().__init__()
         self.name = name
         self.zero_out = zero_out
         self.slope_th = slope_th
+        self.individual_rhs = individual_rhs
+        self.error_metrics = RegressionMetrics(individual_rhs=individual_rhs)
+        self.error_metrics_veg = RegressionMetrics(individual_rhs=individual_rhs)
 
+    def reset(self):
+        self.error_metrics.reset()
+        self.error_metrics_veg.reset()
+    
     @torch.no_grad()
     def get_mask(self, lc, slope):
         zero_cls = torch.tensor([ESA_WC['Built-up'], ESA_WC['Snow and ice'], ESA_WC['Permanent water bodies']], device=lc.device)
@@ -27,32 +70,11 @@ class MaskedLoss(nn.BCELoss):
         loss_mask_slope = loss_mask_slope.type(torch.bool)
         return label_mask, loss_mask_slope
     
-    def error_metrics(self, residuals):
-        res_square = residuals**2
-        rmse_rh98 = torch.sqrt(torch.mean(res_square[:, 98]))
-        me_rh98 = torch.mean(residuals[:, 98])
-        rmse_rh100 = torch.sqrt(torch.mean(res_square[:, 100]))
-        me_rh100 = torch.mean(residuals[:, 100])
-        mse = torch.mean(res_square)
-        me = torch.mean(residuals)
-        mae = torch.mean(torch.abs(residuals))
-        rmse = torch.sqrt(mse)
-        return {
-            "mse": mse,
-            "me": me,
-            "mae": mae,
-            "rmse": rmse,
-            "rmse_rh98": rmse_rh98,
-            "me_rh98": me_rh98,
-            "rmse_rh100": rmse_rh100,
-            "me_rh100": me_rh100,
-        }
-    
-    def forward(self,  rhs_hat, rhs, lc, slope, latlon, sens, shot_number,training:bool=True) -> Tensor:
+    def forward(self,  rhs_hat, rhs, lc, slope, latlon, sens, shot_number,predict_high_slope:bool=False) -> Tensor:
         label_mask, loss_mask = self.get_mask(lc, slope)
         if self.zero_out:
             rhs = rhs * label_mask.unsqueeze(-1)
-        if training:
+        if not predict_high_slope:
             rhs_hat = rhs_hat[loss_mask][..., 7, 7].unsqueeze(-1)
             rhs = rhs[loss_mask].float().unsqueeze(-1)
             center_lc = lc[loss_mask,..., 7, 7]
@@ -68,8 +90,8 @@ class MaskedLoss(nn.BCELoss):
         veg_mask = torch.where((center_lc==50)|(center_lc==70) | (center_lc==80), 0, 1) # built-up, snow and ice, permanent water bodies
         veg_mask = veg_mask.bool()
         residuals = rhs_hat - rhs
-        error_metrics = self.error_metrics(residuals)
-        error_metrics_veg = self.error_metrics(residuals[veg_mask])
+        error_metrics = self.error_metrics.update(residuals)
+        error_metrics_veg = self.error_metrics_veg.update(residuals[veg_mask])
         error_metrics['loss'] = error_metrics[self.name]
         output = {
             'loss': error_metrics['loss'],

models/losses/quantile_ce_loss.py
@@ -17,7 +17,7 @@ class QuantileCELoss(MaskedLoss):
         self.quantiles = quantiles
 
 
-    def forward(self,  y_hat, rhs, lc, slope, latlon, sens, shot_number, training:bool=True) -> Tensor:
+    def forward(self,  y_hat, rhs, lc, slope, latlon, sens, shot_number, predict_high_slope:bool=False) -> Tensor:
         if isinstance(self.quantiles, list):
             quantiles_tensor = torch.tensor(self.quantiles, device=y_hat.device).view(1, -1)
         else:
@@ -31,7 +31,7 @@ class QuantileCELoss(MaskedLoss):
         lc = lc//10
         lc[lc==0.95] = 11
         lc = lc.long()
-        if training:
+        if not predict_high_slope:
             rhs_hat = y_hat[loss_mask, 12:, 7, 7] # (n, 303)
             rhs = rhs[loss_mask].unsqueeze(-1)
             center_lc = lc[loss_mask,..., 7, 7]

models/losses/quantile_loss.py
@@ -16,7 +16,7 @@ class QuantileLoss(MaskedLoss):
         self.quantiles = quantiles
 
 
-    def forward(self,  rhs_hat, rhs, lc, slope, latlon, sens, shot_number, training:bool=True) -> Tensor:
+    def forward(self,  rhs_hat, rhs, lc, slope, latlon, sens, shot_number, predict_high_slope:bool=False) -> Tensor:
         if isinstance(self.quantiles, list):
             quantiles_tensor = torch.tensor(self.quantiles, device=rhs_hat.device).view(1, -1)
         else:
@@ -30,7 +30,7 @@ class QuantileLoss(MaskedLoss):
 
         if self.zero_out:
             rhs = rhs * label_mask.unsqueeze(-1)
-        if training:
+        if not predict_high_slope:
             rhs_hat = rhs_hat[loss_mask][..., 7, 7].unsqueeze(-1)
             rhs = rhs[loss_mask].float().unsqueeze(-1)
             center_lc = lc[loss_mask,..., 7, 7]
@@ -45,11 +45,12 @@ class QuantileLoss(MaskedLoss):
         veg_mask = torch.where((center_lc==5)|(center_lc==7) | (center_lc==8), 0, 1) # built-up, snow and ice, permanent water bodies
         veg_mask = veg_mask.bool()
 
+        # rhs = rhs[:, 98:99]
         n, feature_size, _ = rhs.shape # NOTE: n <= batch_size
         rhs_hat = rhs_hat.reshape(n, feature_size, -1) # n, 101, 3
         residuals = rhs_hat - rhs
         error_metrics = self.error_metrics(residuals[..., median_idx])
-        error_metrics_veg = self.error_metrics(residuals[veg_mask,..., median_idx])
+        error_metrics_veg = self.error_metrics_veg(residuals[veg_mask,..., median_idx])
         quantile_losses = torch.max((quantiles_tensor - 1) * residuals, quantiles_tensor * residuals)
         quantile_loss = torch.mean(torch.sum(quantile_losses, dim=2))
         error_metrics['loss'] = quantile_loss