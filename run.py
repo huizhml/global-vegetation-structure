28754d9f3d50b0a5ee72a0bfb19211814aa8a677
Hui Zhang <omegazhanghui@gmail.com>
2025-04-25 17:10:31 +0200

    Bias correction working

--- run.py
+++ run.py
@@ -274,7 +274,6 @@ class MyLightningCLI(LightningCLI):
                 print(f"num_training_steps: {num_training_steps}")
 
 
-
         if run_id is not None:
             import wandb
             model_alias = 'best'
