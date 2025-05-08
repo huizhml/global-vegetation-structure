30b5fbdea1d893a20da073a1572a04a2fff5ac92
Hui Zhang <omegazhanghui@gmail.com>
2025-05-08 20:37:50 +0200

    disable drop_last to get complete val/test/cal data

--- datasets/_6_visual_check.py
+++ datasets/_6_visual_check.py
@@ -25,6 +25,7 @@ def aggregate_gedi_by_biome(beton_fps: List[str], group_by_biome=False):
     Aggregate GEDI data (RHs + GEDI attributes) {train*}.beton to a single parquet file
     '''
     beton_fps = glob.glob(str(Path(beton_fps).expanduser()))
+    print(beton_fps)
     cols = [f'rh{i}' for i in range(101)] + ['wc', 'slope', 'lon', 'lat']
     data_dir = Path(beton_fps[0]).parent.parent
     ecoregions = gpd.read_file( '~/data/GEDI/ecoregions/wwf_terr_ecos.shp')
@@ -36,7 +37,7 @@ def aggregate_gedi_by_biome(beton_fps: List[str], group_by_biome=False):
             continue
         loader = Loader(fp, batch_size=batch_size, num_workers=1,
                         distributed=False, batches_ahead=3,
-                        order=OrderOption.SEQUENTIAL, os_cache=False)
+                        order=OrderOption.SEQUENTIAL, os_cache=False, drop_last=False)
 
         data = []
         for batch in tqdm(loader):
@@ -44,6 +45,7 @@ def aggregate_gedi_by_biome(beton_fps: List[str], group_by_biome=False):
             batch_ = np.concatenate([rhs, wc[...,7:8,7], slope[...,7:8,7], lon[:,7:8], lat[:, 7:8]], axis=1)
             data.append(batch_)
         data = np.concatenate(data, axis=0)
+        print(data.shape)
         df = pd.DataFrame(data, columns=cols)
         df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.lon, df.lat))
         df = df.set_crs(epsg=4326)
