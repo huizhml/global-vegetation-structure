ac3e7ced6496586ad86791aeae0495d4ad6426e1
Hui Zhang <omegazhanghui@gmail.com>
2025-05-08 20:31:59 +0200

    save prediction of specified RHs - sparse evaluation

--- callbacks/prediction_logger.py
+++ callbacks/prediction_logger.py
@@ -1,197 +1,35 @@
 import torch
-from torch import Tensor
-from typing import Any, List, Union, Optional, Mapping
-from lightning import LightningModule, Trainer
+from typing import Any, List, Union
 from lightning.pytorch.callbacks.callback import Callback
-import wandb
-import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
-
-from const import ESA_WC_s
-from .utils import check_if_log
-
-
-
-def plot_prediction(pred: Tensor, target: Tensor, xlabel:str='Relative Height (0-100)', **kwargs: Any):
-    fig = plt.figure(figsize=(12, 6))
-    plt.plot(pred.cpu().numpy())
-    plt.scatter(target.cpu().numpy(), label='target')
-    plt.xticks(np.arange(101, 10))
-    plt.xlabel(xlabel)
-    plt.ylabel('RH')
-    if pred.shape[1] > 1:
-        plt.legend([f'Q{i+1}' for i in range(pred.shape[1])] + ['target'])
-    else:
-        plt.legend(['pred', 'target'])
-    return fig
-
-SHOT_NUMBERS_TRAIN = [
-    # wc 10
-    201400200300352775, 
-    # wc 20
-    98870300300351318, 
-    # wc 30
-    65781100200204486, 
-    # wc 40
-    165320500300301582, 
-    # wc 50
-    76760000200246936, 
-    # wc 60
-    88020800100038631, 
-    # wc 70
-    91200000100032552, 
-    # wc 80
-    135990300200106060, 
-    # wc 90
-    44160000400290624, 
-    # wc 95
-    207121100100113911, 
-    # wc 100
-    127420600200503637, 
-]
-
-SHOT_NUMBERS_VAL = [
-    # debug
-    90651100400337024, 
-    45930000300182392,
-    # wc 10
-    44350500300290822, 
-    # wc 20
-    31100200100114853,
-    # wc 30
-    49620000400452946,
-    # wc 40
-    22170200300146877, 
-    # wc 50
-    136650500100095174,
-    # wc 60
-    50960500300181616, 
-    # wc 70
-    145771100200196539,
-    # wc 80
-    165870800200125841,
-    # wc 90
-    133020500200055572,
-    # wc 95
-    199640600400585013, 
-    # wc 100
-    214911100300236035,
-]
 
 
 class PredictionLogger(Callback):
 
     def __init__(self,
-                 log_batch:int=0,
-                 log_every: Union[int, List]=None,
-                 show_n_examples:int=10,
-                 shot_numbers_train:List[int]=SHOT_NUMBERS_TRAIN,
-                 shot_numbers_val:List[int]=SHOT_NUMBERS_VAL,
+                 output_rh_idxs: List[int]=None,
                  **kwargs: Any) -> None:
         super().__init__(**kwargs)
-        self.log_batch = log_batch
-        self.log_every = log_every
-        self.show_n_examples = show_n_examples
-        self.shot_numbers_train = shot_numbers_train
-        self.shot_numbers_val = shot_numbers_val
-        self.finished = []
-
-
-    @torch.no_grad()
-    def on_train_batch_end(self, trainer: Trainer, pl_module: LightningModule, outputs, batch: Any, batch_idx: int) -> None:
-        current_epoch = trainer.current_epoch
-        if len(self.finished) == len(self.shot_numbers_train): # all logged, skip the rest batches in this epoch
-            return super().on_train_batch_end(trainer, pl_module, outputs, batch, batch_idx)
-        shot_numbers = [v for v in self.shot_numbers_train if v in outputs['shot_number']]
-        self.finished += shot_numbers
-        if check_if_log(current_epoch, self.log_every) and len(shot_numbers) > 0:
-            # fig = plot_prediction(outputs['pred'], outputs['target'])
-            # wandb.log({f'Prediction [train]': wandb.Image(fig)})
-            # plt.close(fig)
-            #NOTE: Currently visualized examples are from slope<=20
-            lc = outputs['lc'][..., 7,7].cpu().numpy() 
-            if outputs.get('lc_pred') is not None:
-                lc_pred = outputs['lc_pred'][..., 7,7].cpu().numpy()
-            for i in shot_numbers:
-                idx = torch.where(outputs['shot_number']==i)[0]
-                pred = torch.cat([outputs['rhs_hat'][idx], outputs['rhs'][idx]], dim=2).cpu().numpy()
-                n_features = outputs['rhs_hat'].shape[1]
-                n_predictions = outputs['rhs_hat'].shape[2]
-                slope = outputs['slope'][idx,7,7].cpu().numpy().round(2)
-                if outputs.get('lc_pred') is not None:
-                    lc_pred_name = ESA_WC_s[lc_pred[idx].item()].replace('/', '_')
-                else:
-                    lc_pred_name = None
-                lc_name = ESA_WC_s[lc[idx].item()].replace('/', '_')
-                title = f'[train] epoch{current_epoch}/{lc_name}-pred-{lc_pred_name}_slope{round(slope.item(), 2)} sample{i}'
-                line = wandb.plot.line_series(
-                            xs=range(n_features),
-                            ys=pred[0].T,
-                            keys=[f'Q{k}' for k in range(n_predictions)] + ['target'],
-                            xname='Relative Height (0-100)',
-                            title=title)
-                wandb.log({f'prediction/{title}': line})
-        return super().on_train_batch_end(trainer, pl_module, outputs, batch, batch_idx)
-
-    @torch.no_grad()
-    def on_train_epoch_start(self, trainer: Trainer, pl_module: LightningModule) -> None:
-        self.finished = []
-        return super().on_train_epoch_end(trainer, pl_module)
-    
-    # @torch.no_grad()
-    # def on_validation_epoch_start(self, trainer: Trainer, pl_module: LightningModule) -> None:
-    #     self.finished = []
-    #     return super().on_validation_epoch_end(trainer, pl_module)
-    
-
-    # @torch.no_grad()
-    # def on_validation_batch_end(self, trainer: Trainer, pl_module: LightningModule, outputs, batch: Any, batch_idx: int, dataloader_idx: int = 0) -> None:
-    #     current_epoch = trainer.current_epoch
-    #     if len(self.finished) == len(self.shot_numbers_val): # all logged, skip the rest batches in this epoch
-    #         return super().on_train_batch_end(trainer, pl_module, outputs, batch, batch_idx)
-    #     shot_numbers = [v for v in self.shot_numbers_val if v in outputs['shot_number']]
-    #     self.finished += shot_numbers
-    #     if check_if_log(current_epoch, self.log_every) and len(shot_numbers) > 0:
-    #         #NOTE: Currently visualized examples are from slope<=20
-    #         lc = outputs['lc'][..., 7,7].cpu().numpy() 
-    #         if outputs.get('lc_pred') is not None:
-    #             lc_pred = outputs['lc_pred'][..., 7,7].cpu().numpy()
-    #         for i in shot_numbers:
-    #             idx = torch.where(outputs['shot_number']==i)[0]
-    #             pred = torch.cat([outputs['rhs_hat'][idx], outputs['rhs'][idx]], dim=2).cpu().numpy()
-    #             n_features = outputs['rhs_hat'].shape[1]
-    #             n_predictions = outputs['rhs_hat'].shape[2]
-    #             slope = outputs['slope'][idx,7,7].cpu().numpy().round(2)
-    #             if outputs.get('lc_pred') is not None:
-    #                 lc_pred_name = ESA_WC_s[lc_pred[idx].item()].replace('/', '_')
-    #             else:
-    #                 lc_pred_name = None
-    #             lc_name = ESA_WC_s[lc[idx].item()].replace('/', '_')
-    #             title = f'[val] epoch{current_epoch}/{lc_name}-pred-{lc_pred_name}_slope{round(slope.item(), 2)} sample{i}'
-    #             line = wandb.plot.line_series(
-    #                         xs=range(n_features),
-    #                         ys=pred[0].T,
-    #                         keys=[f'Q{k}' for k in range(n_predictions)] + ['target'],
-    #                         xname='Relative Height (0-100)',
-    #                         title=title)
-    #             wandb.log({f'prediction/{title}': line})
-    #     return super().on_validation_batch_end(trainer, pl_module, outputs, batch, batch_idx, dataloader_idx)
-
+        
+        self.output_cols = [f'RH{i}' for i in output_rh_idxs] + ['RH95_GEDI', 'RH98_GEDI', 'RH100_GEDI', 'slope_mask', 'veg_mask']
     
     def on_test_epoch_start(self, trainer, pl_module):
         self.canopy_heights = []
+        self.outfile_suffix = '_corrected' if pl_module.correct_bias else ''
     
     @torch.no_grad()
     def on_test_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx = 0):
-        self.canopy_heights.append(outputs.cpu())
+        y_hat = outputs[0][:, self.output_rh_idxs]
+        canopy_heights = torch.cat([y_hat, outputs[1:]], dim=1)
+        self.canopy_heights.append(canopy_heights.cpu())
         return super().on_test_batch_end(trainer, pl_module, outputs, batch, batch_idx, dataloader_idx)
     
 
     def on_test_epoch_end(self, trainer, pl_module):
         self.canopy_heights = torch.cat(self.canopy_heights)
-        df = pd.DataFrame(self.canopy_heights, columns=['RH95_ours', 'RH98_ours', 'RH100_ours', 'RH95_GEDI', 'RH98_GEDI', 'RH100_GEDI', 'slope_mask', 'veg_mask'])
-        df.to_parquet(f'output/canopy_height_predictions_{trainer.logger._experiment.id}.parquet')
+        df = pd.DataFrame(self.canopy_heights, columns=self.output_cols)
+        df.to_parquet(f'output/canopy_height_predictions_{trainer.logger._experiment.id}{self.outfile_suffix}.parquet')
+        print(f'saved canopy height predictions to output/canopy_height_predictions_{trainer.logger._experiment.id}{self.outfile_suffix}.parquet')
         return super().on_test_epoch_end(trainer, pl_module)
     
 
--- models/_base_pl_model.py
+++ models/_base_pl_model.py
@@ -5,24 +5,23 @@ from lightning import LightningModule
 import torchmetrics
 from models.metrics import MAE, RMSE, MAPE, ME
 
-from models.modules.util import get_nonveg_mask
+from models.modules.util import get_veg_mask
 from const import LAT_MEAN, LAT_STD, LON_SIN_MEAN, LON_SIN_STD, LON_COS_MEAN, LON_COS_STD, SLOPE_MEAN, SLOPE_STD
-
 
 
 class BaseModel(LightningModule):
 
     def __init__(
-            self, 
+            self,
             feed_slope: bool = False,
             zero_slope: bool = False,
-            slope_th: float = 20, 
-            zero_out_nonveg:bool=False,
+            slope_th: float = 20,
+            zero_out_nonveg: bool = False,
             filter_out_nonveg: bool = False,
             feed_latlon: bool = False,
             evaluate_high_slope: bool = False,
-            loss_fc: nn.Module = None, 
-            transform: nn.Module = None, 
+            loss_fc: nn.Module = None,
+            transform: nn.Module = None,
             yhat_transform: nn.Module = None, *args: Any, **
             kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
@@ -38,107 +37,86 @@ class BaseModel(LightningModule):
         self.yhat_transform = yhat_transform
         self.train_metrics = torchmetrics.MetricCollection(
             {
-               'MAE/': MAE(),
+                'MAE/': MAE(),
                 'RMSE/': RMSE(),
                 'ME/': ME(),
-                **{ f'MAE_h>{i}/': MAE(threshold=i) for i in [30,40, 50]},
-                **{ f'RMSE_h>{i}/': RMSE(threshold=i) for i in [30,40, 50]},
-                **{ f'ME_h>{i}/': ME(threshold=i) for i in [30,40, 50]}
+                **{f'MAE_h>{i}/': MAE(threshold=i) for i in [30, 40, 50]},
+                **{f'RMSE_h>{i}/': RMSE(threshold=i) for i in [30, 40, 50]},
+                **{f'ME_h>{i}/': ME(threshold=i) for i in [30, 40, 50]}
                 # # 'mape': MAPE()
-            }, compute_groups=False, # update process is different for these metrics
+            }, compute_groups=False,  # update process is different for these metrics
             postfix='--- train'
         )
         self.val_dataname = 'val'
         self.val_metrics = self.train_metrics.clone(postfix=f'--- {self.val_dataname}')
         self.val_metrics_veg = self.train_metrics.clone(postfix=f'--- {self.val_dataname} --- veg')
         self.val_metrics_lcc = torchmetrics.Accuracy('multiclass', num_classes=12, multidim_average='global')
-        if self.evaluate_high_slope: # for bias correction
+        if self.evaluate_high_slope:  # for bias correction
             self.val_me_with_steep_slope = ME()
         self.save_hyperparameters()
 
-    # def on_validation_start(self):
-    #     if self.trainer.world_size == 1 and self.current_epoch == 0: # only for validate, test and predictions
-    #         # self.val_dataname = f'--- {self.trainer.datamodule.val_fp.stem}'
-    #         self.val_metrics.postfix = f'--- {self.val_dataname}'
-    #         self.val_metrics_veg.postfix = f'--- {self.val_dataname} --- veg'
+        # Predefine processing functions based on flags
+        if self.filter_out_nonveg and self.zero_out_nonveg:
+            raise ValueError('filter_out_nonveg and zero_out_nonveg cannot be both True')
+        self.process_slope = self._process_slope if self.feed_slope else lambda x, slope_arr: x
+        self.process_latlon = self._process_latlon if self.feed_latlon else lambda x, lat, lon: x
+        self.process_nonveg = self._process_nonveg if self.zero_out_nonveg else lambda rhs, veg_mask: rhs
+        self.add_veg_mask = self._add_veg_mask if self.filter_out_nonveg else lambda veg_mask, slope_mask: slope_mask
 
-   
-
-    # def configure_model(self):
-    #     if self.model is not None:
-    #         return
-
-    #     for module in self.modules():
-    #         if isinstance(module, (nn.TransformerEncoderLayer, nn.TransformerDecoderLayer)):
-    #             fully_shard(module, mesh=self.device_mesh)
-
-    #     fully_shard(model, mesh=self.device_mesh)
-
-    #     self.model = torch.compile(self.encoder)
 
     def get_slope_mask(self, slope):
         loss_mask_slope = torch.where(slope < self.slope_th, 1, 0)
         return loss_mask_slope.type(torch.bool)
 
-    
+    def _process_slope(self, x, slope_arr):
+        if self.zero_slope:
+            slope = torch.zeros_like(slope_arr)
+        else:
+            slope = torch.nan_to_num(slope_arr, nan=0)
+        slope = torch.nan_to_num(slope_arr, nan=0)
+        slope = (slope - SLOPE_MEAN) / SLOPE_STD
+        return torch.cat([x, slope.unsqueeze(1)], dim=1)
+
+    def _process_latlon(self, x, lon, lat):
+        lon = lon.unsqueeze(1).repeat(1, 15, 1)
+        lat = lat.unsqueeze(2).repeat(1, 1, 15)
+        sin_lon = torch.sin(lon * torch.pi / 180)
+        cos_lon = torch.cos(lon * torch.pi / 180)
+        lat = (lat - LAT_MEAN) / LAT_STD
+        sin_lon = (sin_lon - LON_SIN_MEAN) / LON_SIN_STD
+        cos_lon = (cos_lon - LON_COS_MEAN) / LON_COS_STD
+        return torch.cat([x, lat.unsqueeze(1), sin_lon.unsqueeze(1), cos_lon.unsqueeze(1)], dim=1)
+
+    def _process_nonveg(self, rhs, veg_mask):
+        return rhs * veg_mask.unsqueeze(-1)
+
+    def _add_veg_mask(self, veg_mask, slope_mask):
+        return (veg_mask & slope_mask).bool()
+
     def training_step(self, sample, batch_idx):
         x = self.transform(sample[0])
         rhs = sample[1]
-        # print(rhs.mean())
         slope_mask = self.get_slope_mask(sample[3][..., 7, 7])
-        if self.filter_out_nonveg:
-            veg_mask = get_nonveg_mask(sample[2])
-            slope_mask = (veg_mask & slope_mask).bool()
-        if self.zero_out_nonveg:
-            label_mask = get_nonveg_mask(sample[2])
-            rhs = rhs * label_mask.unsqueeze(-1)
-        if self.feed_slope:
-            slope = torch.nan_to_num(sample[3], nan=0) 
-            slope = (slope - SLOPE_MEAN) / SLOPE_STD
-            x = torch.cat([x, slope.unsqueeze(1)], dim=1)
-        if self.feed_latlon:
-            lon = sample[4].unsqueeze(1).repeat(1, 15, 1)
-            lat = sample[5].unsqueeze(2).repeat(1, 1, 15)
-            sin_lon = torch.sin(lon*torch.pi/180)
-            cos_lon = torch.cos(lon*torch.pi/180)
-            lat = (lat - LAT_MEAN) / LAT_STD
-            sin_lon = (sin_lon - LON_SIN_MEAN) / LON_SIN_STD
-            cos_lon = (cos_lon - LON_COS_MEAN) / LON_COS_STD
-            x = torch.cat([x,lat.unsqueeze(1), sin_lon.unsqueeze(1), cos_lon.unsqueeze(1)], dim=1)
-        y_hat = self.forward(x.float()) # (n, 303, 15, 15)
+        veg_mask = get_veg_mask(sample[2]) # NOTE: make all masks based on the raw batch size
+        # NOTE: veg mask will filter out nonveg samples if filter_out_nonveg is True
+        mask = self.add_veg_mask(veg_mask, slope_mask)
+        # NOTE: this step will zero out nonveg samples if zero_out_nonveg is True
+        rhs = self.process_nonveg(rhs, veg_mask)
+        # NOTE: this step will add slope as input if feed_slope is True, sample[3] is the slope array
+        x = self.process_slope(x, sample[3])
+        # NOTE: this step will add lat and lon as input if feed_latlon is True, sample[4] and sample[5] are the lat and lon vectors
+        x = self.process_latlon(x, sample[4], sample[5])
+        y_hat = self.forward(x.float())  # y_hat is (n, 303, 15, 15)
 
-        losses, pred, target = self.loss_fc(y_hat, slope_mask, rhs, *sample[2:])
+        losses, pred, target = self.loss_fc(y_hat, mask, rhs, *sample[2:])
 
-        rhs = target['rhs'][slope_mask].squeeze()
-        rhs_hat = pred['rhs_hat'][slope_mask].squeeze()
+        # NOTE: training metrics will only be computed on the masked samples, that is, samples that are used for optimization
+        rhs = target['rhs'][mask].squeeze()
+        rhs_hat = pred['rhs_hat'][mask].squeeze()
         self.train_metrics(rhs_hat, rhs)
         for name, loss in losses.items():
             self.log(f'train.{name}', loss, on_epoch=True, on_step=False, sync_dist=True)
         return losses
-
-    # def on_before_optimizer_step(self, optimizer):
-    #     sch = self.lr_schedulers()
-    #     opt = self.optimizers()
-    #     import ipdb; ipdb.set_trace()
-    #     print(f'lr: {opt.param_groups[0]["lr"]}')
-    #     print(sch.get_lr())
-
-
-
-    # def lr_scheduler_step(self, scheduler, metric):
-    #     re = super().lr_scheduler_step(scheduler, metric)
-    #     sch = self.lr_schedulers()
-    #     opt = self.optimizers()
-    #     import ipdb; ipdb.set_trace()
-    #     print(f'lr: {opt.param_groups[0]["lr"]}')
-    #     print(sch.get_lr())
-    #     return re
-
-
-    # def on_train_epoch_end(self):
-    #     self.log_dict(self.train_metrics.compute(), on_epoch=True, on_step=False, sync_dist=True)
-    #     self.trainer.train_dataloader.close()
-    #     return super().on_train_epoch_end()
 
     def on_train_epoch_start(self):
         self.train_metrics.reset()
@@ -150,96 +128,86 @@ class BaseModel(LightningModule):
         return super().on_train_epoch_start()
 
     def on_validation_epoch_end(self):
-        if hasattr(self, 'train_metrics'): #TODO
+        if hasattr(self, 'train_metrics'):  # TODO
             self.log_dict(self.train_metrics.compute(), on_epoch=True, on_step=False, sync_dist=True)
-            # self.trainer.train_dataloader.close()
 
         self.log_dict(self.val_metrics.compute(), on_epoch=True, on_step=False, sync_dist=True)
         self.log_dict(self.val_metrics_veg.compute(), on_epoch=True, on_step=False, sync_dist=True)
-        # self.trainer.val_dataloaders.close()
-
 
     def validation_step(self, sample, batch_idx):
-        
         x = self.transform(sample[0])
         rhs = sample[1]
         slope_mask = self.get_slope_mask(sample[3][..., 7, 7])
-        veg_mask = get_nonveg_mask(sample[2])
-        if self.filter_out_nonveg:
-            slope_mask = (veg_mask & slope_mask).bool()
-        if self.feed_slope:
-            if self.zero_slope:
-                slope = torch.zeros_like(sample[3])
-            else:
-                slope = torch.nan_to_num(sample[3], nan=0) 
-                slope = (slope - SLOPE_MEAN) / SLOPE_STD
-            x = torch.cat([x, slope.unsqueeze(1)], dim=1)
-        if self.feed_latlon:
-            lon = sample[4].unsqueeze(1).repeat(1, 15, 1)
-            lat = sample[5].unsqueeze(2).repeat(1, 1, 15)
-            sin_lon = torch.sin(lon*torch.pi/180)
-            cos_lon = torch.cos(lon*torch.pi/180)
-            lat = (lat - LAT_MEAN) / LAT_STD
-            sin_lon = (sin_lon - LON_SIN_MEAN) / LON_SIN_STD
-            cos_lon = (cos_lon - LON_COS_MEAN) / LON_COS_STD
-            x = torch.cat([x,lat.unsqueeze(1), sin_lon.unsqueeze(1), cos_lon.unsqueeze(1)], dim=1)
+        veg_mask = get_veg_mask(sample[2])
+        # NOTE: veg mask will filter out nonveg samples if filter_out_nonveg is True
+        mask = self.add_veg_mask(veg_mask, slope_mask)
+        # NOTE: this step will zero out nonveg samples if zero_out_nonveg is True
+        rhs = self.process_nonveg(rhs, veg_mask)
+        # NOTE: this step will add slope as input if feed_slope is True, sample[3] is the slope array
+        x = self.process_slope(x, sample[3])
+        # NOTE: this step will add lat and lon as input if feed_latlon is True, sample[4] and sample[5] are the lat and lon vectors
+        x = self.process_latlon(x, sample[4], sample[5])
         y_hat = self.forward(x.float())
-        
-        if self.zero_out_nonveg:
-            label_mask = get_nonveg_mask(sample[2])
-            rhs = rhs * label_mask.unsqueeze(-1)
-        losses, pred, target = self.loss_fc(y_hat, slope_mask, rhs, *sample[2:])
+
+        losses, pred, target = self.loss_fc(y_hat, mask, rhs, *sample[2:])
+        rhs = target['rhs'].squeeze()
+        rhs_hat = pred['rhs_hat'].squeeze()
+        # Process based on slope evaluation strategy, rhs_hat and rhs are masked if not evaluated on steep slopes
+
+        veg_mask = veg_mask.bool()
+        self.val_metrics(rhs_hat[slope_mask], rhs[slope_mask])
+        veg_slope_mask = (slope_mask & veg_mask).bool()
+        self.val_metrics_veg(rhs_hat[veg_slope_mask], rhs[veg_slope_mask])
         if self.evaluate_high_slope:
-            rhs = target['rhs'].squeeze()
-            rhs_hat = pred['rhs_hat'].squeeze()
-            lc = sample[2]
             self.val_me_with_steep_slope(rhs_hat, rhs)
-        else:
-            rhs = target['rhs'][slope_mask].squeeze()
-            rhs_hat = pred['rhs_hat'][slope_mask].squeeze()
-            lc = sample[2][slope_mask]
-
-
-        if self.filter_out_nonveg:
-            self.val_metrics_veg(rhs_hat, rhs)
-        else:
-            veg_mask = get_nonveg_mask(lc)
-            veg_mask = veg_mask.bool()
-            self.val_metrics(rhs_hat, rhs)
-            self.val_metrics_veg(rhs_hat[veg_mask], rhs[veg_mask])
 
         if pred.get('lc_hat') is not None:
             self.val_metrics_lcc(pred.get('lc_hat'), target.get('lc'))
             self.log(f'{self.val_dataname}.acc', self.val_metrics_lcc, on_epoch=True, on_step=False, sync_dist=True)
-        
+
         for name, loss in losses.items():
             self.log(f'{self.val_dataname}.{name}', loss, on_epoch=True, on_step=False, sync_dist=True)
-        
+
         return {
             'veg_mask': veg_mask,
             'slope_mask': slope_mask,
             **pred, **target,
             'rhs_hat': rhs_hat,
-            'rhs': rhs,  
+            'rhs': rhs,
+            'lat': sample[5][:, 7],
+            'lon': sample[4][:, 7],
         }
-    
+
     def on_predict_epoch_start(self):
-        self.trainer.datamodule.pred_dataset.set_prediction_fname(self.logger._experiment.id)
+        if hasattr(self.trainer.datamodule.pred_dataset, 'tile_id'):
+            self.trainer.datamodule.pred_dataset.set_prediction_fname(self.logger._experiment.id)
+            self.predict_step = self._predict_step_for_large_tile
+        else:            
+            self.predict_step = self._predict_step_for_small_patch
         return super().on_predict_epoch_start()
 
-    def predict_step(self, sample, batch_idx):
+    def _predict_step_for_large_tile(self, sample, batch_idx):
         if self.feed_latlon:
             x = self.transform(sample[0][0])
             x = torch.cat([x, sample[0][1]], dim=1)
         else:
             x = self.transform(sample[0])
         y_hat = self.forward(x.float())
-        self.trainer.datamodule.pred_dataset.write_patch_predictions(y_hat, batch_idx, self.logger._experiment.id)
-
+        self.trainer.datamodule.pred_dataset.write_patch_predictions(y_hat, batch_idx)
+                
+    def _predict_step_for_small_patch(self, sample, batch_idx):
+        x = self.transform(sample[0])
+        x = self.process_latlon(x, sample[3], sample[4])
+        y_hat = self.forward(x.float())
+        self.trainer.datamodule.pred_dataset.write_patch_predictions(y_hat, sample[1], sample[5], sample[6])
+    
     def test_step(self, sample, batch_idx):
+        '''
+        For sparse evaluation
+        '''
         x = self.transform(sample[0])
         slope_mask = self.get_slope_mask(sample[3][..., 7, 7])
-        veg_mask = get_nonveg_mask(sample[2])
+        veg_mask = get_veg_mask(sample[2])
         if self.feed_latlon:
             lon = sample[4].unsqueeze(1).repeat(1, 15, 1)
             lat = sample[5].unsqueeze(2).repeat(1, 1, 15)
@@ -248,23 +216,13 @@ class BaseModel(LightningModule):
             lat = (lat - LAT_MEAN) / LAT_STD
             sin_lon = (sin_lon - LON_SIN_MEAN) / LON_SIN_STD
             cos_lon = (cos_lon - LON_COS_MEAN) / LON_COS_STD
-            x = torch.cat([x,lat.unsqueeze(1), sin_lon.unsqueeze(1), cos_lon.unsqueeze(1)], dim=1)
+            x = torch.cat([x, lat.unsqueeze(1), sin_lon.unsqueeze(1), cos_lon.unsqueeze(1)], dim=1)
         y_hat = self.forward(x.float())
-        # pred = y_hat[:, :303].reshape(-1, 101, 3, 15, 15)
-        # pred_rh100 = pred[:, 100, 1]
-        # print(batch_idx)
-        # print((pred[:, 100, 1] - pred[:, 99, 1]).min())
-        # print('RH100 mean prediction: ', pred_rh100.mean())
-        # print('RH100 min prediction: ', pred_rh100.min())
-        # print('RH100 max prediction: ', pred_rh100.max())
-        # print('mean error: ', (pred_rh100[:, 7,7] - sample[1][:, 100]).mean())
-        # print('mae error: ', (pred_rh100[:, 7,7] - sample[1][:, 100]).abs().mean())
-        # print('rmse error: ', ((pred_rh100[:, 7,7] - sample[1][:, 100])**2).mean().sqrt())
-        # print('------------------------------------------')
-        # import ipdb; ipdb.set_trace()
         y_hat = y_hat[:, :303, 7, 7].reshape(-1, 101, 3)
         canopy_heights = y_hat[:, [95, 98, 100], 1]
-        canopy_heights = torch.cat([canopy_heights, sample[1][:, [95,98,100]], slope_mask.unsqueeze(1), veg_mask.unsqueeze(1)], dim=1)
+        canopy_heights = torch.cat(
+            [canopy_heights, sample[1][:, [95, 98, 100]],
+             slope_mask.unsqueeze(1),
+             veg_mask.unsqueeze(1)],
+            dim=1)
         return canopy_heights
-    
-
