2a3a24ff2f6edc0d6dff1efb90f96171057210bb
Hui Zhang <omegazhanghui@gmail.com>
2025-09-25 09:34:25 +0200

    Sync all predictions from ERDA

--- run_hendrix_sync.sh
+++ run_hendrix_sync.sh
@@ -11,7 +11,20 @@
 
 MAX_ERDA_SESSIONS=10
 
+
+check_missing_tiles_on_hendrix() {
+    local year=$1
+    local translate_dir="${HOME}/data/GVS/Deploy/translate_flags_${year}"
+    local prediction_dir="${HOME}/data/GVS/Deploy/predictions_${year}"
+
+    all_tiles=$(find "$translate_dir" -name "*_done" -exec basename {} \; | sed -E 's/(_best_images_done|_done)//')
+    existing_tiles=$(find "$prediction_dir" -name "*_cog" -exec basename {} \; | sed -E 's/(_cog)//')
+    missing_tiles=$(comm -23 <(echo "$all_tiles" | sort) <(echo "$existing_tiles" | sort))
+    echo "$missing_tiles"
+}
+
 check_unsynced_tiles() {
+    local year=$1
     local sync_dir="${HOME}/data/GVS/Deploy/sync_flags_${year}"
     local translate_dir="${HOME}/data/GVS/Deploy/translate_flags_${year}"
 
@@ -71,7 +84,7 @@ EOF
 )
     sftp_status=$?
     if [ $sftp_status -eq 0 ] && [ "$remote_count" -ge 303 ]; then
-        echo "[${tile_id}] Remote verification passed (${remote_count}/${predicted_cogs_count}). Cleaning up..."
+        echo "[${tile_id}] Remote verification passed (${remote_count}/${predicted_cogs_count}). Create sync flag..."
         touch "${HOME}/data/GVS/Deploy/sync_flags_${year}/${tile_id}_best_images_done"
     else
         echo "[${tile_id}] Sync failed (sftp exit ${sftp_status}). Remote count: ${remote_count}"
@@ -79,6 +92,25 @@ EOF
 }
 
 case $1 in
+0)
+echo "Syncing all tiles from ERDA"
+year=2024
+job_count=0
+tile_ids=$(check_missing_tiles_on_hendrix $year)
+for tile_id in ${tile_ids[@]}; do
+
+    sync_tile_from_erda $tile_id $year &
+    ((job_count++))
+
+    if [ "$job_count" -ge "$MAX_ERDA_SESSIONS" ]; then
+        wait -n  # Wait for one job to finish before launching more
+        ((job_count--))
+    fi
+done
+wait
+sleep 5
+;;
+
 1)
 echo "Syncing specific tiles from ERDA"
 tile_ids=(32VNK 32VNJ 32VMJ 32VPJ 32VMH 32VNH 32VPH 32UMG 32UNG 32UPG 33UUB 33VUC 32UMF 32UNF 32UPF 38KMC 35NQB 51UWB 32PLT)
@@ -118,8 +150,9 @@ sleep 5
 ;;
 3)
 echo "Syncing all tiles to ERDA"
+year=$2
 while true; do
-    unsynced_tiles=$(check_unsynced_tiles)
+    unsynced_tiles=$(check_unsynced_tiles $year)
     if [ -z "$unsynced_tiles" ]; then
         echo "No tiles to sync. Sleeping..."
         sleep 10
@@ -131,7 +164,7 @@ while true; do
 
     job_count=0
     for tile_id in $unsynced_tiles; do
-        sync_tile_to_erda $tile_id &
+        sync_tile_to_erda $tile_id $year &
         ((job_count++))
 
         if [ "$job_count" -ge "$MAX_ERDA_SESSIONS" ]; then
