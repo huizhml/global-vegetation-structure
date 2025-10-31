5b74a91ef8b66a7934c159b3f176a7bbd6e2e2b7
Hui Zhang <omegazhanghui@gmail.com>
2025-10-31 09:04:31 +0100

    update deploy status

--- deploy/check_prediction_status.py
+++ deploy/check_prediction_status.py
@@ -8,7 +8,9 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 from tqdm import tqdm
+import subprocess
 import re
+import pyarrow.parquet as pq
 
 def check_input_images_availability(s2_grid_file: str):
     s2_grid_file = Path(s2_grid_file).expanduser()
@@ -77,6 +79,104 @@ def check_prediction_status(s2_grid_file: str, flag_dir: str, prediction_dir: st
                 #     df.loc[df['Name'] == tile, f'has_s2_metadata_{year}'] = False
     df.to_parquet(save_dir / f'deploy_status.parquet')
 
+
+def add_gedi_correction_count(s2_grid_file: str, geidi_correction_dir: str):
+    s2_grid_file = Path(s2_grid_file).expanduser()
+    geidi_correction_dir = Path(geidi_correction_dir).expanduser()
+    df = gpd.read_parquet(s2_grid_file)
+    s2_tiles_cover_gedi = np.loadtxt(Path.home() / 'data/gvs/deploy/s2_tiles_covered_by_gedi.txt', dtype=str)
+    df['covered_by_gedi'] = False
+    df.loc[df['Name'].isin(s2_tiles_cover_gedi), 'covered_by_gedi'] = True
+    for year in [2020, 2024]:
+        df[f'gedi_correction_count_{year}'] = 0
+        for tile in df['Name']:
+            file_path = geidi_correction_dir / f'partitions_{year}_v1/{tile}.parquet'
+            if file_path.exists():
+                gedi_correction_count = pq.ParquetFile(file_path).metadata.num_rows
+                df.loc[df['Name'] == tile, f'gedi_correction_count_{year}'] = gedi_correction_count
+    df.to_parquet(s2_grid_file)
+    import ipdb; ipdb.set_trace()
+    
+
+def check_prediction_validity(prediction_files: list[Path], min_size: int=209715200):
+    '''
+    Check if the prediction is valid
+    - number of predictions is 303
+    - smallest prediction size is >= min_size
+    '''
+    num_predictions = len(prediction_files)
+    if prediction_files:
+        smallest_prediction_size = min(f.stat().st_size for f in prediction_files) # 204800 bytes = 200 MB
+    else:
+        smallest_prediction_size = 0
+    return num_predictions == 303 and smallest_prediction_size >= min_size # 204800 bytes = 200 MB
+
+def update_deploy_status(deploy_status_file: str):
+    '''
+    Update column: predicted_{year}
+    '''
+    deploy_status_file = Path(deploy_status_file).expanduser()
+    df = gpd.read_parquet(deploy_status_file)
+    df = df.drop_duplicates(subset=[f'Name'])
+    cog_dir = Path(f'~/data/gvs/deploy/predictions_2020').expanduser()
+    gtiff_dir = Path(f'~/data/gvs/deploy/predictions_GTiff_2020').expanduser()
+    df['type_2020'] = 'cog'
+    predicted_wrong_tiles = pd.read_csv(f'~/data/gvs/deploy/predicted_not_ordered_tiles_2020.txt')
+    duplicate_tiles = pd.read_csv(f'~/data/gvs/deploy/duplicated_s2_tiles.txt')
+    for tile in df['Name']:
+        # check for 2020
+        if tile in predicted_wrong_tiles['Name'].values:
+            df.loc[df['Name'] == tile, f'type_2020'] = 'gtiff'
+            df.loc[df['Name'] == tile, f'predicted_2020'] = True #TODO: assume new prediction are all valid, check number of predictions later
+            # # check number of predictions
+            # prediction_files = list((gtiff_dir / f'{tile}_GTiff').glob('*.tif'))
+            # if check_prediction_validity(prediction_files):
+            #     df.loc[df['Name'] == tile, f'predicted_2020'] = True
+            # else:
+            #     df.loc[df['Name'] == tile, f'predicted_2020'] = False
+                
+        else:
+            prediction_files = list((cog_dir / f'{tile}_cog').glob('*.cog.tif'))
+            if check_prediction_validity(prediction_files, min_size=0):
+                df.loc[df['Name'] == tile, f'predicted_2020'] = True
+            else:
+                df.loc[df['Name'] == tile, f'predicted_2020'] = False
+                # os.system(f'rm {Path.home()/ f'data/gvs/deploy/translate_flags_2020/{tile}_done'}')
+                # os.system(f'rm {Path.home()/ f'data/gvs/deploy/inference_flags_2020/{tile}_best_images_done'}')
+        # check for 2024
+        zone_name = tile[:3].lower()
+        num_predictions = int(subprocess.check_output(
+            f"rclone ls lumi-465001846-private:{zone_name}-2024/predictions_GTiff_2024/{tile} | wc -l",
+                shell=True
+            ).decode("utf-8").split()[0])
+        if num_predictions == 0:
+            smallest_prediction_size = 0
+        else:   
+            smallest_prediction_size = int(subprocess.check_output(
+                f"rclone ls lumi-465001846-private:{zone_name}-2024/predictions_GTiff_2024/{tile} | awk '{{print $1}}' | sort -n | head -n 1",
+                shell=True
+                ).decode("utf-8").split()[0])
+        if num_predictions == 303 and smallest_prediction_size >= 209715200: # 209715200 bytes = 200 MB
+            df.loc[df['Name'] == tile, f'predicted_2024'] = True
+        else:
+            df.loc[df['Name'] == tile, f'predicted_2024'] = False
+            # os.system(f'rm {Path.home()/ f'data/gvs/deploy/flags_inference_2024/{tile}_best_images_done'}')
+    df.to_parquet(deploy_status_file)
+    unpredicted_20 = df[df['predicted_2020'] == False]['Name'].tolist()
+    unpredicted_24 = df[df['predicted_2024'] == False]['Name'].tolist()
+    unpredicted_20 = unpredicted_20 + duplicate_tiles['Name'].tolist()
+    df_20 = pd.DataFrame(unpredicted_20, columns=['Name'])
+    df_20['meta_file_idx_2020'] = pd.NA
+    df_20.to_csv(f'~/data/gvs/deploy/slurm_job_files_2020/deploy_s2_items_2020_without_images.txt', index=False)
+    df_24 = pd.DataFrame(unpredicted_24, columns=['Name'])
+    df_24['meta_file_idx_2024'] = pd.NA
+    df_24.to_csv(f'~/data/gvs/deploy/slurm_job_files_2024/deploy_s2_items_2024_without_images.txt', index=False)
+    print(f'Number of unpredicted 2020 tiles: {len(unpredicted_20)}')
+    print(f'Number of unpredicted 2024 tiles: {len(unpredicted_24)}')
+    print(f'Unpredicted 2020 tiles: {unpredicted_20}')
+    print(f'Unpredicted 2024 tiles: {unpredicted_24}')
+    
+
 def get_unfinished_tiles(deploy_status_file: str):
     deploy_status_file = Path(deploy_status_file).expanduser()
     df = gpd.read_parquet(deploy_status_file)
@@ -100,15 +200,23 @@ class MyConfig:
     flag_dir: str = '~/data/gvs/deploy/translate_flags'
     prediction_dir: str = '~/data/gvs/deploy/predictions'
     save_dir: str = '~/data/gvs/'
+    task: str = 'check_prediction_status'
     
 cs = ConfigStore.instance()
 cs.store(name="config", node=MyConfig)
 
 @hydra.main(config_name='config', version_base='1.2')
 def main(cfg):
-    check_prediction_status(cfg.s2_grid_file, cfg.flag_dir, cfg.prediction_dir, cfg.save_dir)
+    if cfg.task == 'check_prediction_status':
+        check_prediction_status(cfg.s2_grid_file, cfg.flag_dir, cfg.prediction_dir, cfg.save_dir)
+    elif cfg.task == 'update_deploy_status':
+        update_deploy_status(f'{cfg.save_dir}/deploy/deploy_status.parquet')
     # check_input_images_availability(cfg.s2_grid_file)
-    get_unfinished_tiles(f'{cfg.save_dir}/deploy_status.parquet')
+    elif cfg.task == 'get_unfinished_tiles':
+        get_unfinished_tiles(f'{cfg.save_dir}/deploy/deploy_status.parquet')
+    elif cfg.task == 'add_gedi_correction_count':
+        gedi_correction_dir = cfg.get('gedi_correction_dir', '~/data/gvs/GEDI_for_correction')
+        add_gedi_correction_count(cfg.s2_grid_file, gedi_correction_dir)
 
 if __name__ == '__main__':
     main()
