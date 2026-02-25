9abbf5c7548499c73b420799b99292004d58bfec
Hui Zhang <omegazhanghui@gmail.com>
2026-02-25 13:27:39 +0100

    feat: add functionality for calculating and translating TIFF differences
    - Enhanced the run_visualization.sh script to include a new option for calculating the difference between two TIFF files using gdal_calc.py.
    - Added functionality to translate the output TIFF to COG format and clean up temporary files, improving data processing efficiency.

--- scripts/run_visualization.sh
+++ scripts/run_visualization.sh
@@ -20,7 +20,7 @@ case $1 in
 # ----------------------------------------
 #    Resample and mosaic the predictions
 # ----------------------------------------
-python -m visualization.run run=resample_and_mosaic run.rh_idx=${2:-98}
+python -m visualization.run run=resample_and_mosaic run.rh_idx=${2:-98} run.q_idx=${3:-1}
 ;;
 1.1)
 # ----------------------------------------
@@ -31,6 +31,29 @@ python -m visualization.run run=create_global_diff_mosaic \
           run.left_q_idx=${3:-0} \
           run.right_q_idx=${4:-2}
 ;;
+
+2)
+# ----------------------------------------
+#    Calculate the difference between two tif files
+# ----------------------------------------
+data_root_dir=${HOME}/data/gvs/predictions/2020/blended/mosaic/
+gdal_calc.py -A ${data_root_dir}/${2}.tif -B ${data_root_dir}/${3}.tif --outfile=${data_root_dir}/${4}.tif \
+    --calc="A-B" --format=GTiff \
+    --co="TILED=YES" \
+    --co="COPY_SRC_OVERVIEWS=YES" \
+    --co="COMPRESS=LERC_ZSTD" \
+    --type='Int16' \
+    --NoDataValue=32767 \
+    --overwrite
+
+gdal_translate ${data_root_dir}/${4}.tif ${data_root_dir}/${4}.cog.tif \
+    -of COG \
+    -co COMPRESS=LERC_ZSTD \
+    -co MAX_Z_ERROR=0
+    
+rm ${data_root_dir}/${4}.tif
+;;
+
 *)
 echo "Invalid option"
 exit 1
